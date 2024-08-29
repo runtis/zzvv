a19e1f8e72e144a80ef3dfed232908a1962e4ff5
runtis <toperdima2005@mail.ru>
2024-08-29 23:25:58 +0300

    memory usage of vectorVoid

--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -17,4 +17,14 @@ typedef struct vectorVoid {
     size_t baseTypeSize;
 } vectorVoid;
 
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+void clearV(vectorVoid *v);
+
+void shrinkToFitV(vectorVoid *v);
+
+void deleteVectorV(vectorVoid *v);
+
 #endif //UNTITLED1_VECTORVOID_H
