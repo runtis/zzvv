3c05a0322584c522c5474ce381ebc7ee75541db6
runtis <toperdima2005@mail.ru>
2024-08-30 06:05:22 +0300

    Matrix getNSpecialElement2

--- main.c
+++ main.c
@@ -10,72 +10,42 @@
 #include "libs/data_structures/matrix/matrix.h"
 #include "libs/algorithms/algorithm.h"
 
-int get_matrix_norm(matrix m) {
-    int res = 0;
+int getNSpecialElement2(matrix m) {
+    int result = 0;
     for (int i = 0; i < m.nRows; i++) {
         for (int j = 0; j < m.nCols; j++) {
-            int norm = abs(m.values[i][j]);
-            if (norm > res) {
-                res = norm;
+            bool is_lefts_less = true;
+            for (int g = j - 1; g >= 0; g--) {
+                if (m.values[i][g] >= m.values[i][j]) {
+                    is_lefts_less = false;
+                }
+            }
+            if (is_lefts_less) {
+                bool is_rights_more = true;
+                for (int g = j + 1; g < m.nCols; g++) {
+                    if (m.values[i][g] <= m.values[i][j]) {
+                        is_rights_more = false;
+                    }
+                }
+                if (is_rights_more) {
+                    result++;
+                }
             }
         }
     }
-    return res;
+    return result;
 }
 
-void print_matrices_with_min_norm(matrix *ms, int n) {
-    if (n > 0) {
-        int *norms = malloc(sizeof(int)*n);
-        norms[0] = get_matrix_norm(ms[0]);
-        int min_norm = norms[0];
-        for (int i = 1; i < n; i++) {
-            norms[i] = get_matrix_norm(ms[i]);
-            if (norms[i] < min_norm) {
-                min_norm = norms[i];
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (norms[i] == min_norm) {
-                outputMatrix(ms[i]);
-            }
-        }
-    }
-}
-
-void test_print_matrices_with_min_norm_1() {
-    matrix *ms = getMemArrayOfMatrices(0, 0, 0);
-    print_matrices_with_min_norm(ms, 0);
-}
-
-void test_print_matrices_with_min_norm_2() {
-    matrix m1 = createMatrixFromArray((int[]) {2, 1,
-                                               1, 3},2, 2);
-    matrix m2 = createMatrixFromArray((int[]) {1, 1,
-                                               2, 1},2, 2);
-    matrix m3 = createMatrixFromArray((int[]) {3, 4,
-                                               1, 5},2, 2);
-    matrix m4 = createMatrixFromArray((int[]) {4, 4,
-                                               4, 1},2, 2);
-    matrix m5 = createMatrixFromArray((int[]) {2, 1,
-                                               1, 2},2, 2);
-    matrix *ms = getMemArrayOfMatrices(5, 3, 2);
-    ms[0] = m1;
-    ms[1] = m2;
-    ms[2] = m3;
-    ms[3] = m4;
-    ms[4] = m5;
-    print_matrices_with_min_norm(ms, 5);
-    freeMemMatrices(ms, 5);
-}
-
-void test_print_matrices_with_min_norm() {
-    test_print_matrices_with_min_norm_1();
-    test_print_matrices_with_min_norm_2();
+void test_getNSpecialElement2() {
+    matrix m = createMatrixFromArray((int[]) {2, 3, 5, 5, 4,
+                                              6, 2, 3, 8, 12,
+                                              12, 12, 2, 1, 2},3, 5);
+    assert(getNSpecialElement2(m) == 4);
+    freeMemMatrix(&m);
 }
 
 int main() {
-    test_print_matrices_with_min_norm();
+    test_getNSpecialElement2();
 
     return 0;
 }
