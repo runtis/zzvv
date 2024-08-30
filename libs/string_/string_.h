78dd68341dddc7b63e166c196e528139015a3ea7
runtis <toperdima2005@mail.ru>
2024-08-30 08:34:27 +0300

    string getword

--- libs/string_/string_.h
+++ libs/string_/string_.h
@@ -11,6 +11,15 @@
 #include <assert.h>
 #include <memory.h>
 
+#define MAX_STRING_SIZE 100
+#define MAX_N_WORDS_IN_STRING 100
+#define MAX_WORD_SIZE 20
+
+typedef struct WordDescriptor {
+    char *begin;
+    char *end;
+} WordDescriptor;
+
 size_t strlen_(const char *begin);
 
 char* find(char *begin, char *end, int ch);
@@ -35,6 +44,28 @@ void test_string();
 
 void removeNonLetters(char *s);
 
+void removeExtraSpaces(char *s);
 
+void removeAdjacentEqualLetters(char *s);
+
+void test_removeAdjacentEqualLetters();
+
+void test_removeExtraSpaces_1();
+
+int getWord(char *beginSearch, WordDescriptor *word);
+
+void digitToStart(WordDescriptor word);
+
+void digitToEnd(WordDescriptor word);
+
+void digitToEndAndReverse(WordDescriptor word);
+
+void digits_to_end(char *string);
+
+void digits_to_end_and_reverse(char *string);
+
+void letters_to_end(char *string);
+
+void test_WordDescriptor();
 
 #endif //UNTITLED1_STRING__H
